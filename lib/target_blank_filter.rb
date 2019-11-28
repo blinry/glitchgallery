184cdb49692757521dde0c3436cf48366ab60341
Sebastian Morr <sebastian@morr.cc>
2019-11-28 16:12:07 +0100

    Filter links to target=_blank

---
+++ lib/target_blank_filter.rb
@@ -0,0 +1,14 @@
+class AbsolutizePathsFilter < Nanoc::Filter
+    identifier :target_blank
+    type :text
+
+    def run(content, params={})
+        content.gsub(/\]\(([^\/].+?)\)/) do
+            if $1.include?'://'
+                "](#{$1}){:target=\"_blank\"}"
+            else
+                "](#{$1})"
+            end
+        end
+    end
+end
